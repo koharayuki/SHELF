d805a328b75e5eadec4f7541f01622071014f2ad
koharayuki <aquarius3106@icloud.com>
2023-06-09 23:11:36 +0900

    一覧表示機能/記事投稿機能の実装(単体テスト)

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :article do
-    
+    title                  { 'タイトル' }
+    text                { '説明' }
+    genre_id            { 3 }
+
+    association :user
+
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @article = FactoryBot.build(:article)
+  end
+
+  describe '記事投稿' do
+    context '投稿できるとき' do
+      it '必要事項(title、text, genre_id)が過不足なく存在すれば登録できる' do
+        expect(@article).to be_valid
+      end
+    end
+
+    context '商品出品ができないとき' do
+      it 'titleが空では登録できない' do
+        @article.title = ''
+        @article.valid?
+        expect(@article.errors.full_messages).to include("Title can't be blank")
+      end
+
+      it 'textが空では登録できない' do
+        @article.text = ''
+        @article.valid?
+        expect(@article.errors.full_messages).to include("Text can't be blank")
+      end
+
+      it 'category_idが1では登録できない' do
+        @article.genre_id = 1
+        @article.valid?
+        expect(@article.errors.full_messages).to include("Genre can't be blank")
+      end
+    end
+  end
 end
