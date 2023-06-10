4c3e606514aa943853c1d2c4afe87e9d4afe47f7
koharayuki <aquarius3106@icloud.com>
2023-06-10 18:09:58 +0900

    詳細表示機能の実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :authenticate_user!, except: :index
+  before_action :move_to_index, only: :edit
 
   def index
     @articles = Article.order("created_at DESC")
@@ -18,9 +19,19 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   private
 
   def article_params
     params.require(:article).permit(:title,:text,:genre_id,:URL).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
