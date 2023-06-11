0b3b7700dcb43d7b10d52fa4ee5611ebf8fbe8e0
koharayuki <aquarius3106@icloud.com>
2023-06-11 16:00:14 +0900

    README/DB設計などの部分修正
    ・不要な記述の削除
    ・README/DB設計の更新
    ・”テスト用ファイル”の変更

--- README.md
+++ README.md
@@ -90,7 +90,7 @@ Things you may want to cover:
 - belongs_to :follower, class_name: "User"
 
 
-## categoriesテーブル
+## genresテーブル
 
 |  Column            |  Type      |  Options                       |
 | ------------------ | ---------- | ------------------------------ |
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
   resources :articles, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   resources :users do
     resource :follows, only: [:create, :destroy]
-    get 'followings' => 'follows#followings', as: 'followings'
-    get 'followers' => 'follows#followers', as: 'followers'
   end
 
   resources :users, only: [:index, :show]
