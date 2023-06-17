557a9b010c6e337bba315c8d104a864e7070f1de
koharayuki <aquarius3106@icloud.com>
2023-06-18 02:24:12 +0900

    routes.rbのリファクタリング

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "articles#index"
-  resources :articles, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   resources :users do
     resource :follows, only: [:create, :destroy]
     get 'followings' => 'follows#followings', as: 'followings'
     get 'followers' => 'follows#followers', as: 'followers'
-  end
-
-  resources :users do
     resources :articles do
       resources :favorites, only: :index
     end
