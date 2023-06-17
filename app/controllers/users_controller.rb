b6b86a17aa942f7e8b083595f071d77b2f5c1065
koharayuki <aquarius3106@icloud.com>
2023-06-18 02:41:47 +0900

    ログアウトからのアクセス制限実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
+  before_action :move_to_index, only: :show
 
   def show
     @user = User.find(params[:id])
     @articles = @user.articles
     @article = Article.new
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to root_path
+    end
   end
 end
