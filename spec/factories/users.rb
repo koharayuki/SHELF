5d9cc48d54c176a06b35a19f7db0f63763d84f86
koharayuki <aquarius3106@icloud.com>
2023-06-18 13:18:34 +0900

    userモデルの単体テスト(追加および変更)
    ・エラーメッセージを日本語に変更したため
    ・ユーザー新規登録時にimage必須としたため

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,9 @@ FactoryBot.define do
     email                 { Faker::Internet.free_email }
     password              { '1a' + Faker::Internet.password(min_length: 4) }
     password_confirmation { password }
+
+    after(:build) do |user|
+      user.image.attach(io: File.open('public/images/test_image.jpg'), filename: 'test_image.jpg')
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe User, type: :model do
 
   describe 'ユーザー新規登録' do
     context '新規登録できるとき' do
-      it '必要事項(nickname,email、password,password_confirmation)が過不足なく存在すれば登録できる' do
+      it '必要事項(nickname,email、password,password_confirmation,image)が過不足なく存在すれば登録できる' do
         expect(@user).to be_valid
       end
     end
@@ -16,13 +16,13 @@ RSpec.describe User, type: :model do
       it 'nicknameが空では登録できない' do
         @user.nickname = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+        expect(@user.errors.full_messages).to include("ニックネームを入力してください")
       end
 
       it 'emailが空では登録できない' do
         @user.email = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Email can't be blank")
+        expect(@user.errors.full_messages).to include("Eメールを入力してください")
       end
 
       it '重複したemailが存在する場合は登録できない' do
@@ -30,54 +30,60 @@ RSpec.describe User, type: :model do
         another_user = FactoryBot.build(:user)
         another_user.email = @user.email
         another_user.valid?
-        expect(another_user.errors.full_messages).to include("Email has already been taken")
+        expect(another_user.errors.full_messages).to include("Eメールはすでに存在します")
       end
 
       it 'emailは@を含まないと登録できない' do
         @user.email = 'testmail'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Email is invalid")
+        expect(@user.errors.full_messages).to include("Eメールは不正な値です")
       end
 
       it 'passwordが空では登録できない' do
         @user.password = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password can't be blank", "Password is invalid", "Password confirmation doesn't match Password")
+        expect(@user.errors.full_messages).to include("パスワードを入力してください", "パスワードは不正な値です", "パスワード（確認用）とパスワードの入力が一致しません")
       end
 
       it 'passwordが6文字未満では登録できない' do
         @user.password = '00000'
         @user.password_confirmation = '00000'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)", "Password is invalid")
+        expect(@user.errors.full_messages).to include("パスワードは6文字以上で入力してください", "パスワードは不正な値です")
       end
 
       it 'passwordで半角英字のみでは登録できない' do
         @user.password = 'aaaaaa'
         @user.password_confirmation = 'aaaaaa'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is invalid")
+        expect(@user.errors.full_messages).to include("パスワードは不正な値です")
       end
 
       it 'passwordで半角数字のみでは登録できない' do
         @user.password = '000000'
         @user.password_confirmation = '000000'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is invalid")
+        expect(@user.errors.full_messages).to include("パスワードは不正な値です")
       end
 
       it '全角文字を含むパスワードでは登録できない' do
         @user.password = 'ａａa １１1'
         @user.password_confirmation = 'ａａa １１1'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is invalid")
+        expect(@user.errors.full_messages).to include("パスワードは不正な値です")
       end
 
       it 'passwordとpassword_confirmationが不一致では登録できない' do
         @user.password = '123456'
         @user.password_confirmation = '1234567'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password", "Password is invalid")
+        expect(@user.errors.full_messages).to include("パスワード（確認用）とパスワードの入力が一致しません", "パスワードは不正な値です")
+      end
+
+      it 'imageが空では登録できない' do
+        @user.image = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("画像を入力してください")
       end
     end
   end
